d07b9f64bbd7772722db223782e2eb16ca93ddaa
root <root@srinivasan-300E4Z-300E5Z-300E7Z.(none)>
2013-12-09 21:30:32 +0530

    FirstCommit

---
+++ maximumSumbarray.py
@@ -0,0 +1,15 @@
+#  Maximum Sub Seqeuence Sum Problem 
+#  Problem Definition -- Given the an array of A1,A2,A3....An.... Caculate the max sum of the sequence...
+
+# Three classic solution are given for the problem and all testimonials how to design an  algorihtm
+
+
+# O(N2) soultion given below
+
+
+
+def naiveMaxSubarray(a[])
+	for i in range(0,9)
+		
+
+
